d033a3dbd489dc71b3a881ff07cc1713fba6e117
Tyler and George <tbennett88@gmail.com>
2023-09-21 18:42:56 +0000

    Uploading map and test_map

---
+++ src/levelup/map.py
@@ -0,0 +1,23 @@
+#from levelup.position import Position
+
+class GameMap():
+    #starting_position = Position(0,0) # we want this to be random in the future
+    def __init__(self):
+       self.numPostions = 100
+       #self.map_size = 10 # had to think of this in map size
+    def getPositions():
+        pass
+    def calculatePosition(startingPostion, direction):
+        pass
+    def getTotalPositions():
+        pass
+    # adding display so I can visually see a map we can comment out the print
+    """def display_map(self, character_name):
+        for y in range(self.map_size -1, -1, -1):
+            row = []
+            for x in range(self.map_size):
+                if character_name.x == x and character_name.y == y:
+                    row.append('X')
+                else:
+                    row.append('.')
+            print(' '.join(row))"""
---
+++ tests/test_map.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+from levelup.map import GameMap
+#from levelup.position import Position
+
+class TestGameMap(TestCase):
+    def test_init(self):
+        testobj = GameMap()
+
