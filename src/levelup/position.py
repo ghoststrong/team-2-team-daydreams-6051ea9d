e038f7aff8ece9c36c5ce6d464b930d4e55755c1
Matt Soderberg <matthew.soderberg@wellsfargo.com>
2023-09-21 18:12:57 +0000

    clean up of position.py

--- src/levelup/position.py
+++ src/levelup/position.py
@@ -1,8 +1,5 @@
 class Position:
     coordinates = ()
-    #def __init__(self, xcoordinate, ycoordinate):
-    #    pass
-        #coordinates = tuple(xcoordinate, ycoordinate)
     
     def __init__(self, coordinates):
         self.coordinates = coordinates
