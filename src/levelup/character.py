fe505efbb1c02e3e9bec84ff3710ad1e7bc982eb
RyanAndSean <ryanmoffett1@gmail.com>
2023-09-22 13:49:05 +0000

    refactor character class and test to be more clear, get rid of unnecessary method for getName

--- src/levelup/character.py
+++ src/levelup/character.py
@@ -4,8 +4,7 @@ class Character:
     def __init__(self, character_name):
         self.name = character_name
 
-    def getName(self):
-        return self.name
+ 
 
 
 
--- tests/test_character.py
+++ tests/test_character.py
@@ -1,16 +1,14 @@
 from unittest import TestCase
 from levelup.character import Character
 
-class TestCharacterInitWithName(TestCase):
-    def test_init(self):
-        ARBITRARY_NAME = "MyName"
-        testobj = Character(ARBITRARY_NAME)
-        self.assertEqual(ARBITRARY_NAME, testobj.name)
+
+class TestCharacter(TestCase):
+    ARBITRARY_NAME = "Ryan"
+    
+    def test_initialize(self):
+        testobject = Character(ARBITRARY_NAME)
+        self.assertEqual(ARBITRARY_NAME, testobject.name)
 
 
-class TestCharacterGetName(TestCase):
-    def test_init(self):
-        TEST_NAME = "Ryan"
-        testobj = Character(TEST_NAME)
-        result = testobj.getName()
-        self.assertEqual(TEST_NAME, result)
+
+
