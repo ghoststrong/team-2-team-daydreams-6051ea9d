4db959a5474bbeb43dfbdec093c844f045aaaaa8
RyanAndSean <ryanmoffett1@gmail.com>
2023-09-22 15:47:38 +0000

    starting tests of character for enter_map with a fake_map and initial implementation of charter class enter_map method

--- tests/test_character.py
+++ tests/test_character.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 from levelup.character import Character
+from fake_map import FakeMap
 
 
 class TestCharacter(TestCase):
@@ -9,6 +10,10 @@ class TestCharacter(TestCase):
         testobject = Character(self.ARBITRARY_NAME)
         self.assertEqual(self.ARBITRARY_NAME, testobject.name)
 
-
+    def test_entermap(self):
+        testobject = Character(self.ARBITRARY_NAME)
+        fakemap = FakeMap()
+        testobject.enter_map(fakemap)
+        self.assertEqual(testobject.current_position, fakemap.starting_position)
 
 
