5519af8f9d95521c6fd31653a5a5b1498e069144
RyanAndSean <ryanmoffett1@gmail.com>
2023-09-22 15:47:07 +0000

    starting tests of character for enter_map with a fake_map and initial implementation of charter class enter_map method

--- src/levelup/character.py
+++ src/levelup/character.py
@@ -1,10 +1,14 @@
 class Character:
     name = ""
+    current_position = []
 
     def __init__(self, character_name):
         self.name = character_name
 
- 
+    def enter_map(self, map):
+        self.current_position = map.starting_position
+    
+        
 
 
 
---
+++ tests/fake_map.py
@@ -0,0 +1,6 @@
+class FakeMap():
+    x = 3
+    y = 4
+
+    starting_position = [x,y]
+
