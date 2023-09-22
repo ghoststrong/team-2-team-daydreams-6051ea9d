661eb624d50f08998d10a2d0aa8d1cb92f5e646e
RyanAndSean <ryanmoffett1@gmail.com>
2023-09-22 16:38:39 +0000

    update fake_map

--- tests/fake_map.py
+++ tests/fake_map.py
@@ -1,7 +1,9 @@
+from levelup.map import Map
+from levelup.position import Position
+from levelup.controller import Direction
+
 class FakeMap():
     x = 3
     y = 4
 
     starting_position = [x,y]
-
-#
